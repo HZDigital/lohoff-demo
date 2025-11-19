d84226d66aa3893cb991b026eda7ec33e1a9f9c2
Leon Zagorac <87634748+waiwha-project@users.noreply.github.com>
2025-11-19 10:00:51 +0100

    Landing Background Img

--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,7 +1,15 @@
 export function LandingPage() {
+
+  const backgroundStyle = {
+    backgroundImage: "url(https://www.lohoff.com/acapsy/img/bg-image.jpg)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat"
+  }
+
   return (
-    <div className="w-full h-full">
-      
+    <div className="w-full h-full" style={backgroundStyle}>
+
     </div>
   )
 }
